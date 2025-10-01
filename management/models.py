5621b779c5fa9302a563d3f7fb73c4423bc21d4c
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-10-01 06:42:50 +0600

    adding AccessToken model

--- management/models.py
+++ management/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import random, string
 
-# Create your models here.
+TOKEN_LENGTH = 8 
+
+def generate_token(length=TOKEN_LENGTH):
+    while True:
+        PossibleToken = string.digits
+        token = ''.join(random.choices(PossibleToken, k=length))
+        if not AccessToken.objects.filter(key=token).exists():
+            return token
+
+class AccessToken(models.Model):
+    token = models.CharField(max_length=100, default=generate_token, unique=True)
+    role = models.Choices(
+        ('staff', 'Staff'),
+        ('doctor', 'Doctor'),
+        ('display', 'Display')
+    )
+    role = models.CharField(max_length=20, choices=role.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.token
