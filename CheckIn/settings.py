938c0b4d1edbc4796d4c07abacfe1a2feb9c6751
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-10-07 04:58:43 +0600

    udpateing my settings

--- CheckIn/settings.py
+++ CheckIn/settings.py
@@ -31,12 +31,25 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'unfold',
+    "unfold.contrib.filters",
+    "unfold.contrib.forms",
+    "unfold.contrib.inlines",
+    "unfold.contrib.import_export",
+    "unfold.contrib.guardian",
+    "unfold.contrib.simple_history",
+    "unfold.contrib.location_field",
+    "unfold.contrib.constance",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'management',
+    
 ]
 
 MIDDLEWARE = [
