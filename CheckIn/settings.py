494ed509a76efa082800a2158c088989cb604f97
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-10-08 05:25:55 +0600

    adding jwt

--- CheckIn/settings.py
+++ CheckIn/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'management',
+    'rest_framework_simplejwt',
+
     
 ]
 
@@ -133,3 +135,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'management.authentication.CustomJWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
