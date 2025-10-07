39312f35b7973285f3f1d576426255148af3535a
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-10-07 06:23:45 +0600

    adding authentication and update AccessToken models

--- management/views.py
+++ management/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+from .models import AccessToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
