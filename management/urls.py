fe79c7d48630568d26087c9e8ee3faeea20489f2
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-10-16 01:51:42 +0600

    add missing newline at the end of the urls.py file

--- management/urls.py
+++ management/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from .views import PatientView, DoctorView, QueueView, LoginView
 
 
 urlpatterns = [
-
+    path('login/', LoginView.as_view(), name='login'),
+    path('patients/', PatientView.as_view(), name='patient-list'),
+    path('doctors/', DoctorView.as_view(), name='doctor-list'),
+    path('queue/', QueueView.as_view(), name='queue-list'),
 ]   
