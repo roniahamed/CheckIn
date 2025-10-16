1822d278d1f914a734e7650183980bc49a7010fa
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-10-17 04:30:15 +0600

    feat: implement QueueConsumer for WebSocket handling in task management

---
+++ management/consumers.py
@@ -0,0 +1,29 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import QueueEntry
+from asgiref.sync import sync_to_async
+
+
+class QueueConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+
+        self.room_group_name = 'queue_group'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+    
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+    
+    async def queue_update(self, event):
+        await self.send(text_data=json.dumps({
+            'type':event['type'],
+            'patient':event['patient'],
+        }))
