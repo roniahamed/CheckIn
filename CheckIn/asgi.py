3a56bcf4031f116c8ebea2b89411d8977a00a69c
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-10-18 00:24:06 +0600

    feat: refactor ASGI setup and improve settings imports for better clarity

--- CheckIn/asgi.py
+++ CheckIn/asgi.py
@@ -2,16 +2,20 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import management.routing
-
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CheckIn.settings')
+
+# Initialize Django *before* importing anything that needs models
+django.setup()
+
+from management import routing  # <-- safe to import after setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            management.routing.websocket_urlpatterns
+            routing.websocket_urlpatterns
         )
     ),
 })
